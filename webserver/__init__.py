6add13bece3584c228c9f1e03adc5461a9b0f8eb
Jason Nugraha <jason.nugraha97@gmail.com>
2021-05-22 20:09:08 +0700

    initial commit

---
+++ webserver/__init__.py
@@ -0,0 +1,13 @@
+# INITIATLISING THE APP p
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'rainproject'
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    return app
